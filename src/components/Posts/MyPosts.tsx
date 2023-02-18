ada69e4e7b35f165d4afa6f1a68db73734d9edd1
Maxim Lesev <lesov0iblr@gmail.com>
2023-02-18 16:08:18 +0100

    My posts

---
+++ src/components/Posts/MyPosts.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import account_icon from "../../img/account-icon.svg";
+import './Posts.css'
+
+export function MyPosts(props: any) {
+    return (
+        <div className={'my-posts'}>
+            <div className={'my-posts__box'}>
+                <div className={'my-posts__image'}>
+                    <img src={account_icon} />
+                </div>
+                <p className={'my-posts__text'}>{props.massege}</p>
+            </div>
+            <span>Star: {props.level}</span>
+        </div>
+    );
+}
--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import './Posts.css'
+import {MyPosts} from "./MyPosts";
 
 
 export function Posts() {
     return (
         <div className={'posts'}>
-            <p>There are no posts on the wall</p>
+            <MyPosts massege={'Hi, my name is.'} level={'1'}/>
+            <MyPosts massege={'Im Frontend Developer'} level={'31'}/>
+            <MyPosts massege={'Im love a beautiful girls. You love a girls? --> Like!!!'} level={'100'}/>
         </div>
     )
 }
