daebae516e4704dd10542a914d4326afd7638f50
Maxim Lesev <lesov0iblr@gmail.com>
2023-02-22 22:19:17 +0100

    Route - NavLink

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -8,40 +8,41 @@ import technologies from "../../img/sidebar/Technologies.svg";
 import advertising from "../../img/sidebar/advertising.svg";
 import React from "react";
 import './Sidebar.css'
+import {NavLink} from "react-router-dom";
 
 
 export function Sidebar() {
     return (
         <div className={'sidebar'}>
             <div className={'sidebar__menu'}>
-                <a href={'/content'} className={'sidebar__menu-home-page sidebar__menu-list'}>
+                <NavLink to={'/content'} className={'sidebar__menu-home-page sidebar__menu-list'}>
                     <img className={'sidebar__menu-icon'} src={user}/>
                     <p>Home page</p>
-                </a>
-                <a href={'/news'} className={'sidebar__menu-news sidebar__menu-list'}>
+                </NavLink>
+                <NavLink to={'/news'} className={'sidebar__menu-news sidebar__menu-list'}>
                     <img className={'sidebar__menu-icon'} src={news}/>
                     <p>News</p>
-                </a>
-                <a href={'/acquaintance'} className={'sidebar__menu-acquaintance sidebar__menu-list'}>
+                </NavLink>
+                <NavLink to={'/acquaintance'} className={'sidebar__menu-acquaintance sidebar__menu-list'}>
                     <img className={'sidebar__menu-icon'} src={acquaintance}/>
                     <p>Acquaintance</p>
-                </a>
-                <a href={'/messages'} className={'sidebar__menu-messages sidebar__menu-list'}>
+                </NavLink>
+                <NavLink to={'/messages'} className={'sidebar__menu-messages sidebar__menu-list'}>
                     <img className={'sidebar__menu-icon'} src={messages}/>
                     <p>Messages</p>
-                </a>
-                <a href={'/settings'} className={'sidebar__menu-settings sidebar__menu-list'}>
+                </NavLink>
+                <NavLink to={'/settings'} className={'sidebar__menu-settings sidebar__menu-list'}>
                     <img className={'sidebar__menu-icon'} src={settings}/>
                     <p>Settings</p>
-                </a>
-                <a href={'/developers'} className={'sidebar__menu-developers sidebar__menu-list'}>
+                </NavLink>
+                <NavLink to={'/developers'} className={'sidebar__menu-developers sidebar__menu-list'}>
                     <img className={'sidebar__menu-icon'} src={developers}/>
                     <p>Developers</p>
-                </a>
-                <a href={'/technologies'} className={'sidebar__menu-technologies sidebar__menu-list'}>
+                </NavLink>
+                <NavLink to={'/technologies'} className={'sidebar__menu-technologies sidebar__menu-list'}>
                     <img className={'sidebar__menu-icon'} src={technologies}/>
                     <p>Technologies</p>
-                </a>
+                </NavLink>
             </div>
             <div className={'advertising'}>
                 <h3>Advertising</h3>
