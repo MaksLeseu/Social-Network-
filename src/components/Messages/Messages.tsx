8276038e3e7931852c983c62a881656153671219
Maxim Lesev <lesov0iblr@gmail.com>
2023-02-23 13:37:39 +0100

    Message - Components

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,8 +1,37 @@
 import React from "react";
 import s from './Messages.module.css'
 import logo1 from '../../img/logo1.png'
-import {BrowserRouter, NavLink, Route, Routes} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
+
+type DialogType = {
+    id: string
+    userName: string
+}
+
+type ChatType = {
+    text: string
+}
+
+
+function Dialog(props: DialogType) {
+    return (
+        <NavLink to={'/messages/' + props.id} className={s.dialogs__dialog}>
+            <div className={s.dialog__icon}><img src={logo1}/></div>
+            <span>{props.userName}</span>
+        </NavLink>
+    )
+}
+
+function Chat(props: ChatType) {
+    return (
+        <>
+            <div className={s.block__text}>
+                <span className={s.text}>{props.text}</span>
+            </div>
+        </>
+    )
+}
 
 export function Messages() {
     return (
@@ -10,37 +39,24 @@ export function Messages() {
             <div className={s.messages__dialogs}>
                 <h3>Dialogs</h3>
                 <input className={s.dialogs__search}></input>
-                <NavLink to={'/messages/1'} className={s.dialogs__dialog}>
-                    <div className={s.dialog__icon}><img src={logo1}/></div>
-                    <span>Kathy Pacheco</span>
-                </NavLink>
-                <NavLink to={'/messages/2'} className={s.dialogs__dialog}>
-                    <div className={s.dialog__icon}><img src={logo1}/></div>
-                    <span>Alex Petrov</span>
-                </NavLink>
-                <NavLink to={'/messages/3'} className={s.dialogs__dialog}>
-                    <div className={s.dialog__icon}><img src={logo1}/></div>
-                    <span>Alex Buckmaster</span>
-                </NavLink>
-                <NavLink to={'/messages/4'} className={s.dialogs__dialog}>
-                    <div className={s.dialog__icon}><img src={logo1}/></div>
-                    <span>Nika Rodriges</span>
-                </NavLink>
+                <>
+                    <Dialog userName={'Nik Filman'} id={'1'} />
+                    <Dialog userName={'Mich Filman'} id={'2'} />
+                    <Dialog userName={'Vera Filmana'} id={'3'} />
+                    <Dialog userName={'Vik Prigozin'} id={'4'} />
+                </>
             </div>
             <div className={s.messages__chat}>
                 <div className={s.chat__user}>
                     <div className={s.user__block}>
                         <div className={s.user__icon}><img src={logo1}/></div>
-                        <span>Kathy Pacheco</span>
+                        <span>Nik Filman</span>
                     </div>
                 </div>
                 <div className={s.chat__text}>
-                    <div className={s.block__text}>
-                        <span className={s.text}>Hi brother!</span>
-                    </div>
-                    <div className={s.block__text}>
-                        <span className={s.text}>Ready to take your writing skills.</span>
-                    </div>
+                    <Chat text={'Hi brother!'}/>
+                    <Chat text={'Ready to take your writing skills.'}/>
+                    <Chat text={'А есть что-то другое.'}/>
                 </div>
             </div>
         </div>
